c07f5bd340a36b497c11f77bc22ce1453355699a
PeithonKing <amukherjeeniser@gmail.com>
2025-08-01 19:13:30 +0530

    added more features to jellyfin

---
+++ restart_all.sh
@@ -0,0 +1,55 @@
+# glance
+echo "Restarting Glance..."
+cd glance
+# docker compose down
+time docker compose up -d
+cd ..
+echo ========================================\n\n
+
+# # immich
+# echo "Restarting Immich..."
+# cd immich
+# # docker compose down
+# time docker compose up -d
+# cd ..
+# echo ========================================\n\n
+
+# jellyfin
+echo "Restarting Jellyfin..."
+cd jellyfin
+# docker compose down
+time docker compose up -d
+cd ..
+echo ========================================\n\n
+
+# n8n
+echo "Restarting n8n..."
+cd n8n
+# docker compose down
+time docker compose up -d
+cd ..
+echo ========================================\n\n
+
+# # open-webui
+# echo "Restarting Open WebUI..."
+# cd open-webui
+# # docker compose down
+# time docker compose up -d
+# cd ..
+# echo ========================================\n\n
+
+# pihole
+echo "Restarting PiHole..."
+cd pihole
+# docker compose down
+time docker compose up -d
+cd ..
+echo ========================================\n\n
+
+# speaches
+echo "Restarting Speaches..."
+cd speaches
+# docker compose down
+time docker compose up -d
+cd ..
+echo ========================================\n\n
