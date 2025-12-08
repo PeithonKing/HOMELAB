ec75a3ba2d34206889bef3914fd49ed8455f0d07
PeithonKing <amukherjeeniser@gmail.com>
2025-12-08 11:01:33 +0530

    new containers: portrainer, gitea, frigate, github-runner, and minor tweak to some old ones

--- restart_all.sh
+++ restart_all.sh
@@ -1,3 +1,11 @@
+# pihole
+echo "Restarting PiHole..."
+cd pihole
+# docker compose down
+time docker compose up -d
+cd ..
+echo ========================================\n\n
+
 # glance
 echo "Restarting Glance..."
 cd glance
@@ -22,33 +30,33 @@ time docker compose up -d
 cd ..
 echo ========================================\n\n
 
-# n8n
-echo "Restarting n8n..."
-cd n8n
-# docker compose down
-time docker compose up -d
-cd ..
-echo ========================================\n\n
-
-# # open-webui
-# echo "Restarting Open WebUI..."
-# cd open-webui
+# # n8n
+# echo "Restarting n8n..."
+# cd n8n
 # # docker compose down
 # time docker compose up -d
 # cd ..
 # echo ========================================\n\n
 
-# pihole
-echo "Restarting PiHole..."
-cd pihole
+# open-webui
+echo "Restarting Open WebUI..."
+cd open-webui
 # docker compose down
 time docker compose up -d
 cd ..
 echo ========================================\n\n
 
-# speaches
-echo "Restarting Speaches..."
-cd speaches
+# # speaches
+# echo "Restarting Speaches..."
+# cd speaches
+# # docker compose down
+# time docker compose up -d
+# cd ..
+# echo ========================================\n\n
+
+# portainer
+echo "Restarting portainer..."
+cd portainer
 # docker compose down
 time docker compose up -d
 cd ..
